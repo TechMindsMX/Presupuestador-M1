ea81c6781de478b357dfb0702c57156c0d2bfd6b
carlogilmar <carlogilmar12@gmail.com>
2017-01-26 13:33:58 -0600

    #1 Implementando debugger con findAll

--- src/main/groovy/com/modulus/uno/CustomerController.groovy
+++ src/main/groovy/com/modulus/uno/CustomerController.groovy
@@ -2,15 +2,22 @@ package com.modulus.uno
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView
 
 @Controller
 class CustomerController {
 
+  @Autowired
+  CustomerRepository customerRepository
+
   @RequestMapping("/")
   ModelAndView index() {
+    println "*"*50
+    println customerRepository.findAll()
+    println customerRepository.findByFirstName("Carlo")
+    println "*"*50
     new ModelAndView("index")
   }
 }
