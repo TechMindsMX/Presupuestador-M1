d8e3623ad6a6c1955ed1e50e3e280675d984aa80
carlogilmar <carlogilmar12@gmail.com>
2017-01-26 16:19:02 -0600

    #1 Quitando campo en metodo de CustomerRepository

--- src/main/groovy/com/modulus/uno/CustomerRepository.groovy
+++ src/main/groovy/com/modulus/uno/CustomerRepository.groovy
@@ -4,7 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository
 interface CustomerRepository extends MongoRepository<Customer, String> {
 
   Customer findByFirstName(String firstName)
-  Customer findAllByFirstNameiAndLastName(String firstName String lastName)
   List<Customer> findByLastName(String lastName)
 
 }
