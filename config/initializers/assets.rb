1a7e4e32931dd9a995097bd5b7694a1073931f37
cggg88jorge <jorge@makingdevs.com>
2017-02-10 09:51:12 -0600

    #4 Ajustando la configuracion de los assets de la aplicacion

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,14 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join("vendor","assets","third-party")
+
+Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf )
+
+%w(budget).each do |controller|
+    Rails.application.config.assets.precompile += ["#{controller}.js", "#{controller}.css"]
+end
+
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
