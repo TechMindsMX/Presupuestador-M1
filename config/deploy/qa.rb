a72922321fa06958c95137fde4a1e29fa6da236d
cggg88jorge <jorge@makingdevs.com>
2017-02-13 16:02:52 -0600

    #7 creando archivo para el deploy de qa

---
+++ config/deploy/qa.rb
@@ -0,0 +1,6 @@
+set :stage, :qa
+set :puma_bind, %w(tcp://0.0.0.0:3000)
+set :branch, 'qa'
+set :deploy_to, "/var/www/#{fetch(:application)}/qa"
+
+server 'presupuestador-qa.modulusuno.com', user: 'ec2-user', roles: %w{app web}
