65e9a36adf4969e62e79c05de4c5e141becc7a65
cggg88jorge <jorge@makingdevs.com>
2017-02-13 16:49:27 -0600

    #7 Ajustando el branch el cual se toma para el deploy de qa

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,6 +1,6 @@
 set :stage, :qa
 set :puma_bind, %w(tcp://0.0.0.0:3000)
-set :branch, 'qa'
+set :branch, 'master'
 set :deploy_to, "/var/www/#{fetch(:application)}/qa"
 
 server 'presupuestador-qa.modulusuno.com', user: 'ec2-user', roles: %w{app web}
